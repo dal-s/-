e1898eb131b7fa189857c1bc41318c17307d07e4
dal-s <45187462+dal-s@users.noreply.github.com>
2018-12-19 20:11:53 +0800

    Update binarySearch.go
    多线程版本，存在几个问题。
    1、获取查找结果的函数，当全文查找都没有结果时，程序处于阻塞状态
    2、多线程存在内存泄露问题

--- binarySearch.go
+++ binarySearch.go
@@ -2,6 +2,11 @@ package main
 import (
     "fmt"
     "sort"
+    "os"
+    "io"
+    "bufio"
+    "strings"
+    "strconv"
 )
 
 var p = fmt.Println
@@ -30,10 +35,8 @@ func (a Byage) Less(i, j int) bool {
 func rank(key int,i Byage) int {
     var s=0
     var e=len(i)-1
-    p(e)
     for s <= e {
         var mid = s + (e-s)/2
-        p(key,i[mid].Age)
         if   key < i[mid].Age {
             e=mid-1
         }else if key > i[mid].Age {
@@ -45,17 +48,84 @@ func rank(key int,i Byage) int {
     return -1
 }
 
+func done() int {
+    
+    for {
+        select {
+            case c:= <-succ:
+                if c != -1 {
+                    return c
+                }
+        }
+    }
+    return -1
+}
+
+func readFile() map[string] string {
+    var per=make(map[string] string)
+    var arr []string
+    f,err := os.Open("/home/mart_coo/lijianping1/1.dat")
+    if err != nil {
+        p("read file error! ",err)
+    }
+
+    defer f.Close()
+    br := bufio.NewReader(f)
+    for {
+        a,err := br.ReadString('\n')
+        if err == io.EOF {
+            break
+        }else if err != nil {
+            panic(err)
+        }
+
+        arr =strings.Split(a," ")
+        per[arr[0]]=arr[1]
+    }
+    return per
+}
+
+const threads=10
+var succ =make(chan int)
+
 func main() {
     
-    per := []Person {
-        {"A",31},
-        {"B",20},
-        {"C",48},
-        {"D",18},
-        {"E",19},
-        {"F",122},
+    defer close(succ)
+
+    r :=readFile()
+    tmp :=[10]Person{}
+    per := []Person {}
+
+    for k,v := range r {
+        tmp[0].Name=k
+        tmp[0].Age,_=strconv.Atoi(strings.Replace(v,"\n","",-1))
+        per=append(per,tmp[0])
     }
-    sort.Sort(Byage(per))
-    p(per)
-    p(rank(31,Byage(per)))
+    //p(per)
+    //sort.Sort(Byage(per))
+    //p(per)
+    //p(rank(59361974761705217,Byage(per)))
+    
+    len_data := len(per)
+
+    step := len_data/threads
+
+    if step == 0 {
+        step=len_data
+    }
+
+    p(step)
+    p(len_data)
+
+    for i:=0; i<=len_data-step;i=i+step {
+        go func(data Byage) {
+            sort.Sort(Byage(data))
+            //p("start:",data)
+            ret :=rank(59361974761705217,Byage(data))
+            succ <- ret
+        }(per[i:i+step])
+    }
+
+    c:=done() 
+    p(c)
 }
