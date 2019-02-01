a93d404b7112dd386716930ab355b7184af6eb34
dal-s <45187462+dal-s@users.noreply.github.com>
2019-02-01 10:22:09 +0800

    Update leetcode.go

--- leetcode.go
+++ leetcode.go
@@ -625,8 +625,533 @@ func sortedSquares(A []int) []int {
     sort.Sort(bySquare(A))   
     return A
 }
-func main() {
-    em :=[]int {}
+/*
+    index in a mountain array
+*/
 
-    fmt.Println(sortedSquares(em))
+func peakIndexInMountainArray(A []int) int {
+
+    for k,_ := range A {
+        if A[k+1] < A[k] {
+            return k
+        }
+    }
+    return 0
 }
+/*
+    找出能被自己包含的所有数字取余的数字，如：128 由 1 2 8组成
+*/
+
+func findNumber( i int) []int {
+
+    var array []int
+    for i !=0 {
+        array = append(array,i%10)
+        i = i/10
+    }
+    return array
+}
+
+func selfDividingNumbers(left int, right int) []int {
+
+    var array []int
+    for i:=left;i<=right;i++ {
+        k := i
+        isVaild := true
+        for k !=0 {
+            if num := k%10; num == 0 || i % num != 0{
+                isVaild = false
+                break
+            }
+            k = k/10
+        }
+        if isVaild {
+            array = append(array,i)
+        }
+    }
+    return array
+}
+type byNum []int
+
+func (a byNum) Len() int {
+    return len(a)
+}
+
+func (p byNum) Less(i,j int) bool {
+    return p[i]<p[j]
+}
+
+func (p byNum) Swap (i,j int) {
+    p[i],p[j] = p[j],p[i]
+}
+func arrayPairSum(A []int) int {
+    sort.Sort(byNum(A))   
+    
+    sm := 0
+    for i:=0; i < len(A); i+=2 {
+        sm +=A[i]
+    }
+    return sm
+}
+/*
+    记录每个人调用ping的次数
+*/
+type RecentCounter struct {
+    curName string
+    beforeName []string
+    beforeTm []int   
+    cn int
+    point int
+}
+
+
+func Constructor() RecentCounter {
+    return RecentCounter{} 
+}
+
+const K int = 3000
+func findKey( this *RecentCounter,t int) int {
+
+    fmt.Println("len:",len(this.beforeTm))
+    for i:=this.point;i<len(this.beforeTm);i++ {
+        if this.beforeTm[i]+K >= t {
+            this.point = i
+            fmt.Println("i:",i)
+            return len(this.beforeTm)-i+1
+        }
+    }
+    return 1
+}
+func (this *RecentCounter) Ping(t int) int {
+
+    this.cn=1
+    for _,v := range this.beforeName {
+        if this.curName == v {
+            this.cn = findKey(this,t)
+            break
+        }
+    }
+    this.beforeName=append(this.beforeName,this.curName)
+    this.beforeTm=append(this.beforeTm,t)
+    return this.cn
+}
+/*
+     Fibonacci numbers
+*/
+func fib(N int) int {
+
+    if N == 1 || N == 0{
+        return N
+    }
+    return fib(N-1)+fib(N-2)
+}
+func fib2(N int) int {
+    if N == 0 {return 0}
+    if N == 1 {return 1}
+    
+    a, b := 0, 1
+    for i:=2; i<=N; i++ {
+        a, b = b, a+b
+    }
+    
+    return b
+}
+func sortArrayByParityII(A []int) []int {
+    
+    odd := 1
+    even := 0
+    var sortArray [20000]int
+
+    for i:=0;i<len(A);i++ {
+        if A[i] % 2 ==0 {
+            sortArray[even]=A[i]
+            even +=2
+        }else {
+            sortArray[odd]=A[i]
+            odd +=2
+        }
+    }
+    return sortArray[0:len(A)]
+}
+
+var mp=make(map[string] int)
+func splitString( s string) {
+
+    nmk := strings.Index(s," ")
+    before_num := s[0:nmk]
+    num,_:=strconv.Atoi(before_num)
+    firstPoint := strings.Index(s,".")
+    lastPoint := strings.LastIndex(s,".")
+
+    mp[s[nmk+1:]]=num + mp[s[nmk+1:]]
+    mp[s[firstPoint+1:]]=num + mp[s[firstPoint+1:]]
+
+    if firstPoint != lastPoint {
+        mp[s[lastPoint+1:]]=num + mp[s[lastPoint+1:]]
+    }
+}
+func subdomainVisits(cpdomains []string) []string {
+    var res []string
+    for _,v := range cpdomains {
+        splitString(v)
+    }    
+
+    for k,v := range mp {
+        res = append(res,strconv.Itoa(v)+" "+k)
+    }
+    return res
+}
+
+/*
+revers strings
+*/
+func m(s string) string{
+
+    N := len(s)
+    if N <= 1 {
+        return s
+    }
+
+    a := s[0:N/2]
+    b := s[N/2:N]
+    return m(b)+m(a)
+}
+/*
+    reverse words in a string
+*/
+func reverseString2(s string) string {
+    runes := []rune(s)
+    for from, to := 0, len(runes)-1; from < to; from, to = from+1, to-1 {
+        runes[from], runes[to] = runes[to], runes[from]
+    }
+    return string(runes)
+}
+func reverseWords(s string) string {
+    words := strings.Split(s," ") 
+    var ans string
+    for _,v := range words {
+        ans = ans  + " " + reverseString2(v) 
+    }
+    return ans[1:]
+}
+
+func reverseString(s []byte){
+
+    end := len(s) -1
+    for start := 0;start <len(s)/2;start++ {
+        s[start],s[end] = s[end],s[start]
+        end--
+    }
+}
+/*
+    Number of Lines To Write String
+*/
+func numberOfLines(widths []int, S string) []int {
+    
+    var bts,lines  int
+    for _,v := range S {
+        bts = bts + widths[v-97]
+        if bts == 100 {
+            lines++
+            bts = 0
+        }
+        if bts > 100 {
+            lines++
+            bts = widths[v-97]
+        }
+    }
+    if bts != 0 {
+        lines++
+    }
+    return []int {lines,bts}
+}
+
+func findComplement(num int) int {
+
+    bts := []rune(fmt.Sprintf("%b",num))
+    cn := 0
+    num_len := len(bts)
+    for k:=0;k< num_len;k++ {
+        cn = cn + int(bts[k] ^ 1 - '0')*int(math.Pow(2,float64(num_len - k -1)))
+    }
+    return cn
+}
+func findComplement2(num int) int {
+  h := high(num)
+  mask := 1 << uint(h) -1
+  return mask & (^num)
+}
+
+func high(num int) int {
+  i:=0 
+  for num > 0 {
+    i++
+    num = num >> 1
+  }
+  return i
+}
+
+/*
+
+*/
+func findAlphabet(s string) bool {
+
+    mp := map[rune] int {
+        'q':1,'w':1,'e':1,'r':1,'t':1,'y':1,'u':1,'i':1,'o':1,'p':1,
+        'a':2,'s':2,'d':2,'f':2,'g':2,'h':2,'j':2,'k':2,'l':2,
+        'z':3,'x':3,'c':3,'v':3,'b':3,'n':3,'m':3,
+    }
+
+    row := 0
+    for k,v := range s {
+
+        if v >=65 && v <=90 {
+            v = v+32
+        }
+
+        if k ==0 {
+            row = mp[v]
+        }
+        if v,ok := mp[v]; ok && row != v {
+            return false
+        }
+    }
+    return true
+}
+func findWords(words []string) []string {
+    
+    var ans []string
+    for _,v := range words {
+        if findAlphabet(v) {
+            ans = append(ans,v)
+        }
+    }
+    return ans
+}
+
+func uncommonFromSentences(A string, B string) []string {
+    allString := append(strings.Split(A," "),strings.Split(B," ")...)   
+    ans := allString[:0]
+    var mp=make(map[string] int)
+
+    for _,v := range allString {
+         
+        if _,ok := mp[v] ;ok {
+            mp[v]++
+        }else{
+            mp[v]=1
+        }
+    }
+    for k,v := range mp {
+        if v == 1 {
+            ans = append(ans,k)
+        }
+    }
+    return ans
+}
+
+func islandPerimeter(grid [][]int) int {
+
+    cn := 0
+    for line:=0;line<len(grid);line++ {
+        for row:=0;row<len(grid[0]);row++ {
+            if grid[line][row] == 0 {
+                continue
+            }
+
+            UP    := !(line == 0 ) //false
+            DOWN  := !(line == len(grid) - 1) //false
+
+            LEFT  := !(row == 0 ) //false
+            RIGHT := !(row == len(grid[0]) - 1) //false
+
+            tmp := 4
+
+            if UP {
+                tmp = tmp - grid[line -1][row]
+            }
+            if DOWN {
+                tmp = tmp - grid[line +1][row]
+            }
+            if LEFT {
+                tmp = tmp - grid[line][row -1]
+            }
+            if RIGHT {
+                tmp = tmp - grid[line][row +1]
+            }
+            cn = cn + tmp
+        }
+    }    
+    return cn
+}
+
+func calPoints(ops []string) int {
+    var ans []int
+    cn := 0
+    for _,v := range ops {
+        switch {
+            case v == "+" :
+                ans = append(ans, ans[len(ans)-1] + ans[len(ans)-2])
+            case v == "C" :
+                ans = ans[:len(ans)-1]
+            case v == "D" :
+                ans = append(ans,ans[len(ans)-1]*2)
+            default :
+                point,_:=strconv.Atoi(v) 
+                ans = append(ans,point)
+        }
+    }
+    for _,v := range ans {
+        cn = cn +v
+    }
+    return cn
+}
+
+func min(x,y int) int {
+    if x > y {
+        return y
+    }
+    return x
+}
+func distributeCandies(candies []int) int {
+    candCount := len(candies)   
+
+    var kinds = make(map[int] int)
+
+    for _,kind := range candies {
+        kinds[kind]++
+    }
+
+    return min(len(kinds),candCount/2)
+}
+
+func fizzBuzz(n int) []string {
+    
+    var ans []string
+    for i:=1;i <= n;i++ {
+        mod3 := (i % 3 ==0)
+        mod5 := (i % 5 ==0)
+        if mod3&&mod5 {
+            ans=append(ans,"FizzBuzz")
+            continue
+        }
+        if mod3 {
+            ans=append(ans,"Fizz")
+            continue
+        }   
+        if mod5 {
+            ans=append(ans,"Buzz")
+            continue
+        }
+        ans = append(ans,strconv.Itoa(i))
+    }
+    return ans
+}
+
+func singleNumber(nums []int) int {
+    var singleMp = make(map[int] int)   
+
+    for i:=0;i<len(nums);i++ {
+        if _,ok := singleMp[nums[i]];ok {
+            delete(singleMp,nums[i])
+        }else{
+            singleMp[nums[i]]=0
+        }
+    }
+
+    for k,_ := range singleMp {
+        return k
+    }
+    return 0
+}
+func matrixReshape(nums [][]int, r int, c int) [][]int {
+    
+    original_nums := len(nums)*len(nums[0])
+    var ans [][]int
+    var ans0 []int
+    ans_i :=0
+    ans_k :=0
+    if r*c > original_nums {
+        return nums
+    }
+
+    for i:=0;i<len(nums);i++{
+        for k:=0;k<len(nums[0]);k++{
+            if ans_i > r -1{
+                break
+            }
+            ans0 = append(ans0,nums[i][k])
+            if ans_k >= c -1 {
+                ans = append(ans,ans0)
+                ans_i++
+                ans_k=0
+                ans0 = []int{}
+            }else{
+                ans_k++
+            }
+        }
+    }
+    return ans
+}
+/*
+    自定义排序
+*/
+func isAlienSorted(words []string, order string) bool {
+
+    var orderMp = make(map[byte] int)
+
+    for k,v := range order {
+        orderMp[byte(v)]=k
+    }
+
+    wordsLength := len(words)
+
+    for i:=0;i<wordsLength -1;i++ {
+
+        minWord := min(len(words[i]),len(words[i+1]))
+
+        if len(words[i+1]) < len(words[i]) && words[i][:minWord] == words[i+1] {
+            return false
+        }
+        for k:=0;k<minWord ;k++ {
+            if orderMp[words[i][k]] == orderMp[words[i+1][k]] {
+                continue
+            }else if orderMp[words[i][k]] < orderMp[words[i+1][k]] {
+                break
+            }else {
+                return false
+            }
+        }
+    }
+
+    return true
+}
+
+/*
+    TODO
+*/
+func isTriangle(c,k,g int) (bool,int) {
+    return c<k+g&&k<c+g&&g<c+k,c+k+g
+}
+func largestPerimeter(A []int) int {
+    max := 0
+    sort.Sort(byNum(A))
+    for i:=0;i<=len(A)-3;i++{
+        for j:=i+1;j<=len(A)-2;j++ {
+            for k:=len(A)-1;k>=j+1;k-- {
+                ok,perimeter := isTriangle(A[i],A[j],A[k])
+                if !ok {
+                    break
+                }
+                if perimeter > max{
+                    max = perimeter
+                }
+            }
+        }
+    }
+    return max
+}
+func main() {
+    fmt.Println(largestPerimeter(em))
+}
+
